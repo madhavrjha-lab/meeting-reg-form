395774a635fa19cfc4372acb02b2f0b667ec98d0
Madhav Jha <madhav99jha@gmail.com>
2023-09-06 11:40:03 +0530

    Added QR Select and Report pdf added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 $('.chosen-select').chosen()
+
+document.querySelector('#attendanceTypeInput').addEventListener('change', e => {
+	switch (e.target.value) {
+		case 'QR':
+			document.querySelector('#Form').classList.add('hidden')
+			document.querySelector('#QR').classList.remove('hidden')
+			break
+		case 'Form':
+			document.querySelector('#Form').classList.remove('hidden')
+			document.querySelector('#QR').classList.add('hidden')
+			break
+		default:
+			document.querySelector('#Form').classList.add('hidden')
+			document.querySelector('#QR').classList.add('hidden')
+			break
+	}
+})
